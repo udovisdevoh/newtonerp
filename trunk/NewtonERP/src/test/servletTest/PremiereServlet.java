9ad0adca329087816bd3ac71cd08c6d7a63a63f5
djo0012 <djo0012@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-09-24 00:30:12 +0000

    correct somme package declaration... sa perturbe pascal :P

--- trunk/NewtonERP/src/test/servletTest/PremiereServlet.java
+++ trunk/NewtonERP/src/test/servletTest/PremiereServlet.java
@@ -1,30 +1,39 @@
-package newtonERP.test.servletTest;
+package test.servletTest;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 
  * @author r3hallejo
  * 
- * All right this is not working yet... Stupid question but how to show this in a browser?
- * Taken from : http://www.commentcamarche.net/contents/servlets/servcarac.php3
+ *         All right this is not working yet... Stupid question but how to show
+ *         this in a browser? Taken from :
+ *         http://www.commentcamarche.net/contents/servlets/servcarac.php3
  */
-public class PremiereServlet extends HttpServlet{
-	public void init() {
-	  }
-	  public void doGet(HttpServletRequest req, HttpServletResponse res)
-		throws ServletException, IOException {
-		
-		res.setContentType("text/html");
-		PrintWriter out = res.getWriter();
-		out.println("<HTML>");
-		out.println("<HEAD><TITLE> Titre </TITLE></HEAD>");
-		out.println("<BODY>");
-		out.println("Ma première servlet");
-		out.println("</BODY>");
-		out.println("</HTML>");
-		out.close();
-	  }
+public class PremiereServlet extends HttpServlet
+{
+    public void init()
+    {
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+	    throws ServletException, IOException
+    {
+
+	res.setContentType("text/html");
+	PrintWriter out = res.getWriter();
+	out.println("<HTML>");
+	out.println("<HEAD><TITLE> Titre </TITLE></HEAD>");
+	out.println("<BODY>");
+	out.println("Ma première servlet");
+	out.println("</BODY>");
+	out.println("</HTML>");
+	out.close();
+    }
 }
