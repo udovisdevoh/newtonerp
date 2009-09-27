33d3177ed4b51a283d107525c5e97353c5d81b15
djo0012 <djo0012@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-09-27 21:43:46 +0000

    créé le starter de base pour lancé le serveur le serveur fonction passage du serveur a jetty plutot que tomcat, il est plus léger en plus retrait des librairie de tomcat

---
+++ NewtonERP/src/newtonERP/Starter.java
@@ -0,0 +1,15 @@
+package newtonERP;
+
+import newtonERP.serveur.Servlet;
+
+/**
+ * @author
+ * 
+ */
+public class Starter
+{
+    public static void main(String[] args)
+    {
+	new Servlet(47098);
+    }
+}
--- NewtonERP/src/newtonERP/serveur/Servlet.java
+++ NewtonERP/src/newtonERP/serveur/Servlet.java
@@ -1,21 +1,78 @@
 package newtonERP.serveur;
 
+import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mortbay.jetty.Request;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.AbstractHandler;
 
 /**
- * @author Gabriel Therrien
+ * @author JoCloutier
+ * 
+ *         la gestion du serveur, request manager
  * 
  */
-public class Servlet
+public class Servlet extends AbstractHandler
 {
-    public Servlet()
+    CommandRouteur cmdRouter = new CommandRouteur();
+
+    /**
+     * lance le serveur sur le port indique
+     * 
+     * @param port
+     *            numero de port a utiliser
+     */
+    public Servlet(int port)
     {
-	CommandRouteur theCommandRouteur = new CommandRouteur();
+	// lance le serveur web
+	Server server = new Server(port);
+	server.setHandler(this);
+	try
+	{
+	    server.start();
+	} catch (Exception e)
+	{
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
     }
 
-    public Hashtable<String, String> setCommandRouteurModule(
-	    Hashtable<String, String> allModules)
+    public void handle(String target, HttpServletRequest request,
+	    HttpServletResponse response, int dispatch) throws IOException,
+	    ServletException
     {
-	return allModules;
+	String moduleName;
+	String action;
+	Hashtable<String, String> parameter = new Hashtable<String, String>();
+
+	// on trouve le moduleName et l'actionName dans l'url
+	Matcher urlMatch = Pattern.compile("/(.*)/(.*)").matcher(target);
+	urlMatch.matches();
+	moduleName = urlMatch.group(1);
+	action = urlMatch.group(2);
+
+	// on trouve les parametres pour les mettre dans le hashtable
+	for (Enumeration e = request.getParameterNames(); e.hasMoreElements();)
+	{
+	    String paramName = (String) e.nextElement();
+	    parameter.put(paramName, request.getParameter(paramName));
+	}
+
+	cmdRouter.routeCommand(moduleName, action, parameter);
+
+	// on formatte la réponse
+	response.setContentType("text/html");
+	response.setStatus(HttpServletResponse.SC_OK);
+	response.getWriter().println("<h1>Hello</h1>"); // TODO: replace by
+	// a call to mainViewer
+	((Request) request).setHandled(true);
     }
 }
