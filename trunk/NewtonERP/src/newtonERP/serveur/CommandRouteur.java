ad6e8003282b78a441bb12cdceae821590944016
djo0012 <djo0012@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-09-30 18:25:05 +0000

    pas le temp

--- trunk/NewtonERP/src/newtonERP/serveur/CommandRouteur.java
+++ trunk/NewtonERP/src/newtonERP/serveur/CommandRouteur.java
@@ -16,18 +16,22 @@ public class CommandRouteur
      * 
      * @param moduleName nom du module
      * @param action action a executer
+     * @param moduleGetter la nom du modulGetter a utiliser
      * @param parameter parametre a passer a l'action
      * @return une entity viewable
      */
     public Object routeCommand(String moduleName, String action,
-	    Hashtable<String, String> parameter)
+	    String moduleGetter, Hashtable<String, String> parameter)
     {
 	// TODO: changer Object par viewable quand il va y en avoir
 	Module module;
+	System.out.println(moduleName);
+	System.out.println(action);
+	System.out.println(moduleGetter);
 	try
 	{
 	    module = ListModule.getModule(moduleName);
-	    module.doAction(action, parameter);
+	    module.doAction(action, moduleGetter, parameter);
 	} catch (Exception e)
 	{
 	    // TODO Auto-generated catch block
