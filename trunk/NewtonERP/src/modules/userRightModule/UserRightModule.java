62bfd7cead20c0451238f9a65fe888b39aee127b
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-01 23:00:14 +0000

    Classe UserRightModule faite il faut maintenant implémenter les actions, les moduleGetter et les entités attention, il faut mettre un default module getter pour chaque module

--- trunk/NewtonERP/src/modules/userRightModule/UserRightModule.java
+++ trunk/NewtonERP/src/modules/userRightModule/UserRightModule.java
@@ -1,16 +1,60 @@
 package modules.userRightModule;
 
+import modules.userRightModule.actions.DeleteUser;
+import modules.userRightModule.actions.DeleteUserType;
+import modules.userRightModule.actions.EditUser;
+import modules.userRightModule.actions.EditUserType;
+import modules.userRightModule.actions.GetUserList;
+import modules.userRightModule.actions.GetUserTypeList;
+import modules.userRightModule.actions.NewUser;
+import modules.userRightModule.actions.NewUserType;
+import modules.userRightModule.entityDefinitions.TaskRight;
+import modules.userRightModule.entityDefinitions.User;
+import modules.userRightModule.entityDefinitions.UserType;
+import modules.userRightModule.moduleGetters.TaskRightGetter;
+import modules.userRightModule.moduleGetters.UserGetter;
+import modules.userRightModule.moduleGetters.UserListGetter;
+import modules.userRightModule.moduleGetters.UserTypeGetter;
+import modules.userRightModule.moduleGetters.UserTypeListGetter;
 import newtonERP.module.Module;
+import newtonERP.module.ModuleException;
 
 public class UserRightModule extends Module
 {
 
     /**
-     * Fait Par Gabriel Therrien
+     * Fait Par Gabriel Therrien et Guillaume Lacasse Le module de UserRight
      * 
      * @param args
      */
-    public UserRightModule()
+    public UserRightModule() throws ModuleException
     {
+	// On cré les référence vers les définitions d'entités
+	definitionEntityList.add(new User());
+	definitionEntityList.add(new UserType());
+	definitionEntityList.add(new TaskRight());
+
+	// On cré les références vers les actions du module
+	actionList.put("NewUser", new NewUser());
+	actionList.put("EditUser", new EditUser());
+	actionList.put("DeleteUser", new DeleteUser());
+	actionList.put("NewUserType", new NewUserType());
+	actionList.put("EditUserType", new EditUserType());
+	actionList.put("DeleteUserType", new DeleteUserType());
+	actionList.put("GetUserList", new GetUserList());
+	actionList.put("GetUserTypeList", new GetUserTypeList());
+
+	// On définit l'action par défaut (index)
+	setDefaultAction("GetUserTypeList");
+
+	// On cré les références vers les getters du module
+	moduleGetterList.put("TaskRightGetter", new TaskRightGetter());
+	moduleGetterList.put("UserGetter", new UserGetter());
+	moduleGetterList.put("UserListGetter", new UserListGetter());
+	moduleGetterList.put("UserTypeGetter", new UserTypeGetter());
+	moduleGetterList.put("UserTypeListGetter", new UserTypeListGetter());
+
+	// On défini le le moduleGetter par défaut
+	setDefaultModuleGetter("UserTypeListGetter");
     }
 }
