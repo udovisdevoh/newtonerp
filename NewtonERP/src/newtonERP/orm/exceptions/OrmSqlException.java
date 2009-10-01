611ed8835f1e12535da74659b1a38993a7756605
jonathan.hallee <jonathan.hallee@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-01 21:29:21 +0000

    nouvelle exception

---
+++ NewtonERP/src/newtonERP/orm/exceptions/OrmSqlException.java
@@ -0,0 +1,12 @@
+package newtonERP.orm.exceptions;
+
+/**
+ * 
+ * @author r3hallejo
+ * 
+ *         Basic exception class representing an sql exception that can be
+ *         thrown from the orm
+ */
+public class OrmSqlException extends OrmException
+{
+}
