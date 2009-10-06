7e547efa152a2518ff6d19749f362c66af2d7b88
saluthi <saluthi@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-06 04:11:01 +0000

    crée newgroup pis edituser

---
+++ NewtonERP/src/modules/userRightModule/actions/EditGroup.java
@@ -0,0 +1,24 @@
+package modules.userRightModule.actions;
+
+import java.util.Hashtable;
+
+import newtonERP.module.actions.ActionableEntity;
+import newtonERP.module.actions.IAction;
+import newtonERP.viewers.Viewable;
+
+/**
+ * Fait Par Gabriel Therrien
+ * 
+ * @param args
+ */
+public class EditGroup implements IAction
+{
+
+    @Override
+    public Viewable perform(ActionableEntity entity,
+	    Hashtable<String, String> parameters)
+    {
+	return null;
+    }
+
+}
