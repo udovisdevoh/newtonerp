f8476e7ebc109d22f27356d21ab12279e8764a8b
djo0012 <djo0012@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-07 17:58:18 +0000

    abstractEntity implementant les conversions hashtable / entity

--- trunk/NewtonERP/src/newtonERP/module/AbstractEntity.java
+++ trunk/NewtonERP/src/newtonERP/module/AbstractEntity.java
@@ -1,6 +1,7 @@
 package newtonERP.module;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Hashtable;
 
 /**
@@ -11,41 +12,105 @@ public class AbstractEntity
 {
 
     /**
-     * @param parameters
-     * @return
-     * @throws IllegalAccessException
-     * @throws IllegalArgumentException
+     * @param parameters Hashtable de parametre
      */
-    public AbstractEntity getEntityFromHashTable(
-	    Hashtable<String, String> parameters)
-	    throws IllegalArgumentException, IllegalAccessException
+    public void getEntityFromHashTable(Hashtable<String, String> parameters)
     {
+	String setName;
+	Method methode;
+	try
+	{
+	    for (String key : parameters.keySet())
+	    {
+		setName = "set" + key.substring(0, 1).toUpperCase()
+			+ key.substring(1);
+		methode = getClass().getMethod(
+			setName,
+			new Class[] { getClass().getDeclaredField(key)
+				.getType() });
 
-	return null;
+		if (getClass().getDeclaredField(key).getType()
+			.equals(int.class))
+		    methode.invoke(this, new Object[] { Integer
+			    .parseInt(parameters.get(key)) });
+		else
+		    methode.invoke(this, new Object[] { parameters.get(key) });
+	    }
+	} catch (IllegalArgumentException e)
+	{
+	    throw new IllegalArgumentException(
+		    "le type d'argument ne peut etre gere");
+	} catch (Exception e)
+	{
+	    e.printStackTrace();
+	}
     }
 
     /**
-     * @param entity
-     * @return
-     * @throws Exception
+     * @return Hashtable contenant chaqu'un des champ
      */
-    public Hashtable<String, String> getHashTableFromEntity() throws Exception
+    public Hashtable<String, String> getHashTableFromEntity()
     {
 	Field[] fields = getClass().getDeclaredFields();
-	String fieldName;
+	String getName;
 	String value;
 	Hashtable<String, String> hash = new Hashtable<String, String>();
-	for (int i = 0; i < fields.length; i++)
+	try
 	{
-	    fieldName = "get"
-		    + fields[i].getName().substring(0, 1).toUpperCase()
-		    + fields[i].getName().substring(1);
-	    value = (String) getClass().getMethod(fieldName, null).invoke(this,
-		    null);
-	    hash.put(fieldName, value);
+	    for (int i = 0; i < fields.length; i++)
+	    {
+		getName = "get"
+			+ fields[i].getName().substring(0, 1).toUpperCase()
+			+ fields[i].getName().substring(1);
+		value = getClass().getMethod(getName, null).invoke(this, null)
+			.toString();
+		hash.put(fields[i].getName(), value);
+	    }
+	} catch (Exception e)
+	{
+	    e.printStackTrace();
 	}
 
-	return null;
+	return hash;
+    }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals(Object obj)
+    {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (!(getClass().isInstance(obj)))
+	    return false;
+
+	// vérification field a field
+	Field[] fields = getClass().getDeclaredFields();
+	String getName;
+	String value1;
+	String value2;
+	try
+	{
+	    for (int i = 0; i < fields.length; i++)
+	    {
+		getName = "get"
+			+ fields[i].getName().substring(0, 1).toUpperCase()
+			+ fields[i].getName().substring(1);
+		value1 = getClass().getMethod(getName, null).invoke(this, null)
+			.toString();
+		value2 = getClass().getMethod(getName, null).invoke(this, null)
+			.toString();
+		if (!value1.equals(value2))
+		    return false;
+	    }
+	} catch (Exception e)
+	{
+	    e.printStackTrace();
+	}
+	return true;
     }
 }
