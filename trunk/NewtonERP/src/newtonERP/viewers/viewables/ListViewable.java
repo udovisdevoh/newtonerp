a0b9932b8633f571c4790f9d8cd51e316bc4df64
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-07 16:49:45 +0000

    Interfaces [...]vieable fait

--- trunk/NewtonERP/src/newtonERP/viewers/viewables/ListViewable.java
+++ trunk/NewtonERP/src/newtonERP/viewers/viewables/ListViewable.java
@@ -1,6 +1,35 @@
 package newtonERP.viewers.viewables;
 
+import java.util.HashSet;
+import java.util.Hashtable;
+
+import newtonERP.module.AbstractAction;
+
+/**
+ * @author r3lacasgu
+ * 
+ */
 public interface ListViewable
 {
+    /**
+     * @return Liste des boutons spéficiques (edit, delete) Clef: nom d'un
+     *         bouton Valeur: Action du bouton
+     */
+    public Hashtable<String, AbstractAction> getSpecificActionButtonList();
 
+    /**
+     * @return Liste des boutons globaux (new, exit) Clef: nom d'un bouton
+     *         Valeur: Action du bouton
+     */
+    public Hashtable<String, AbstractAction> getGlobalActionButtonList();
+
+    /**
+     * @return Titre des colonnes
+     */
+    public Iterable<String> getColumnTitleList();
+
+    /**
+     * @return Liste de rangées
+     */
+    public Iterable<HashSet<String>> getRowValues();
 }
--- trunk/NewtonERP/src/newtonERP/viewers/viewables/ProfileViewable.java
+++ trunk/NewtonERP/src/newtonERP/viewers/viewables/ProfileViewable.java
@@ -1,6 +1,32 @@
 package newtonERP.viewers.viewables;
 
+import java.util.Hashtable;
+
+import newtonERP.module.AbstractAction;
+
+/**
+ * @author r3lacasgu
+ * 
+ */
 public interface ProfileViewable
 {
-    // TODO:
+    /**
+     * @return
+     */
+    public String getTitle();
+
+    /**
+     * @return
+     */
+    public String getButtonCaption();
+
+    /**
+     * @return
+     */
+    public AbstractAction getSubmitAction();
+
+    /**
+     * @return Noms des input et leurs valeurs actuelles
+     */
+    public Hashtable<String, String> getInputList();
 }
