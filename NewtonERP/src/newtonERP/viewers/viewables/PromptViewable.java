1a0312a804c2247d5a4c328d44410afa2404e11b
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-07 16:23:28 +0000

    Interface PromptVieable fait

--- NewtonERP/src/newtonERP/viewers/viewables/PromptViewable.java
+++ NewtonERP/src/newtonERP/viewers/viewables/PromptViewable.java
@@ -1,6 +1,20 @@
 package newtonERP.viewers.viewables;
 
+import java.util.Vector;
+
+/**
+ * @author r3lacasgu
+ * 
+ */
 public interface PromptViewable
 {
+    /**
+     * @return nom des input du prompt
+     */
+    public Vector<String> getInputNameList();
 
+    /**
+     * @return nom du bouton "ok"
+     */
+    public String getButtonName();
 }
