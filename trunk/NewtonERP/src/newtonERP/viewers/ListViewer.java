543293c63846a3f4ca62103cd373726981b4ac47
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-07 18:06:40 +0000

    ListViewer in progress

--- trunk/NewtonERP/src/newtonERP/viewers/ListViewer.java
+++ trunk/NewtonERP/src/newtonERP/viewers/ListViewer.java
@@ -2,6 +2,7 @@ package newtonERP.viewers;
 
 import java.util.HashSet;
 
+import newtonERP.module.AbstractAction;
 import newtonERP.viewers.viewables.ListViewable;
 
 public class ListViewer
@@ -20,7 +21,8 @@ public class ListViewer
 
 	html += getHeaderRow(entity.getColumnTitleList());
 
-	html += getDataRowList(entity.getRowValues(), entity);
+	html += getDataRowList(entity.getRowValues(), entity, moduleName,
+		actionName);
 
 	html += "</table>";
 
@@ -28,21 +30,64 @@ public class ListViewer
     }
 
     private static String getDataRowList(Iterable<HashSet<String>> rowValues,
-	    ListViewable entity)
+	    ListViewable entity, String moduleName, String actionName)
     {
 	String html = "<tr>";
 	for (HashSet<String> row : rowValues)
+	{
 	    for (String cell : row)
+	    {
 		html += "<td>" + cell + "</td>";
+	    }
 
-	html += getSpecificButtonList(entity);
+	    html += getSpecificButtonList(entity, moduleName, actionName,
+		    getKeyName(entity.getColumnTitleList()), getKeyValue(row));
+	}
 
 	return html + "</tr>";
     }
 
-    private static String getSpecificButtonList(ListViewable entity)
+    private static String getKeyValue(HashSet<String> row)
     {
-	String html;
+	for (String value : row)
+	    return value;
+	return null;
+    }
+
+    private static String getKeyName(Iterable<String> columnTitleList)
+    {
+	for (String value : columnTitleList)
+	    return value;
+	return null;
+    }
+
+    private static String getSpecificButtonList(ListViewable entity,
+	    String moduleName, String actionName, String key, String value)
+    {
+	String html = "";
+
+	AbstractAction action;
+	for (String buttonCaption : entity.getSpecificActionButtonList()
+		.keySet())
+	{
+	    action = entity.getGlobalActionButtonList().get(buttonCaption);
+	    html += getButton(buttonCaption, moduleName, actionName, key,
+		    value, entity.getCurrentUserName());
+	}
+
+	return html;
+    }
+
+    private static String getButton(String buttonCaption, String moduleName,
+	    String actionName, String key, String value, String currentUserName)
+    {
+	String formActionUrl = "/" + moduleName + "/" + actionName + "?user="
+		+ currentUserName;
+
+	String html = "";
+	html += "<form method=\"GET\" action=\"" + formActionUrl + "\">";
+
+	return html;
     }
 
     public static String getHeaderRow(Iterable<String> columnList)
