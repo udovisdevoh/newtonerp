3ff46a7449ef8b0d0ab306912b6b34fda58869e7
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-07 17:46:36 +0000

    ListViewer in progress

--- NewtonERP/src/newtonERP/viewers/ListViewer.java
+++ NewtonERP/src/newtonERP/viewers/ListViewer.java
@@ -1,5 +1,7 @@
 package newtonERP.viewers;
 
+import java.util.HashSet;
+
 import newtonERP.viewers.viewables.ListViewable;
 
 public class ListViewer
@@ -7,6 +9,47 @@ public class ListViewer
     public static String getHtmlCode(ListViewable entity)
 	    throws ViewerException
     {
-	return "dummy code";
+	String moduleName = entity.getSubmitModule().getClass().getSimpleName();
+	String actionName = entity.getSubmitAction().getClass().getSimpleName();
+
+	String html = "";
+
+	html += "<h1>" + entity.getTitle() + "</h1>";
+
+	html += "<table>";
+
+	html += getHeaderRow(entity.getColumnTitleList());
+
+	html += getDataRowList(entity.getRowValues(), entity);
+
+	html += "</table>";
+
+	return html;
+    }
+
+    private static String getDataRowList(Iterable<HashSet<String>> rowValues,
+	    ListViewable entity)
+    {
+	String html = "<tr>";
+	for (HashSet<String> row : rowValues)
+	    for (String cell : row)
+		html += "<td>" + cell + "</td>";
+
+	html += getSpecificButtonList(entity);
+
+	return html + "</tr>";
+    }
+
+    private static String getSpecificButtonList(ListViewable entity)
+    {
+	String html;
+    }
+
+    public static String getHeaderRow(Iterable<String> columnList)
+    {
+	String html = "<tr>";
+	for (String column : columnList)
+	    html += "<td>" + column + "</td>";
+	return html + "</tr>";
     }
 }
