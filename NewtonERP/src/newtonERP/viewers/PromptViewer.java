6e7751e01e7d0bca888a2455998b4238cfa9e2ae
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-07 16:05:41 +0000

    Viewer (mainViewer) fait (en tant que router vers les viewers spécialisés) il faut maintenant faire les viewers qui output du html et je travaille là dessus pour aujourd'hui

---
+++ NewtonERP/src/newtonERP/viewers/PromptViewer.java
@@ -0,0 +1,12 @@
+package newtonERP.viewers;
+
+import newtonERP.module.AbstractEntity;
+
+public class PromptViewer
+{
+    public static String getHtmlCode(AbstractEntity entity)
+	    throws ViewerException
+    {
+	return "dummy code";
+    }
+}
--- NewtonERP/src/newtonERP/viewers/Viewer.java
+++ NewtonERP/src/newtonERP/viewers/Viewer.java
@@ -1,11 +1,21 @@
 package newtonERP.viewers;
 
 import newtonERP.module.AbstractEntity;
+import newtonERP.viewers.viewables.ListViewable;
+import newtonERP.viewers.viewables.PromptViewable;
 
 public abstract class Viewer
 {
     public static String getHtmlCode(AbstractEntity entity)
+	    throws ViewerException
     {
-	return "dummy code";
+	if (entity instanceof PromptViewable)
+	    return PromptViewer.getHtmlCode(entity);
+	else if (entity instanceof ProfileViewable)
+	    return ProfileViewer.getHtmlCode(entity);
+	else if (entity instanceof ListViewable)
+	    return ListViewer.getHtmlCode(entity);
+
+	throw new ViewerException("Couldn't find proper viewer for entity");
     }
 }
