ffafe7fa15f655b1247fa32eb129c2f41b2878a6
deadcosmonaut <deadcosmonaut@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-16 19:01:32 +0000

    methodes: getHeader(),getLeftMenu(),getFooter()(HTML)

--- trunk/NewtonERP/src/newtonERP/viewers/Viewer.java
+++ trunk/NewtonERP/src/newtonERP/viewers/Viewer.java
@@ -1,5 +1,9 @@
 package newtonERP.viewers;
 
+import java.util.Hashtable;
+import java.util.Iterator;
+
+import newtonERP.ListModule;
 import newtonERP.module.AbstractEntity;
 import newtonERP.viewers.viewables.ListViewable;
 import newtonERP.viewers.viewables.PromptViewable;
@@ -7,7 +11,7 @@ import newtonERP.viewers.viewables.PromptViewable;
 /**
  * @author Guillaume Lacasse, Pascal Lemay
  * 
- * 	Represents the main viewer for our ERP
+ *         Represents the main viewer for our ERP
  */
 public abstract class Viewer
 {
@@ -33,8 +37,85 @@ public abstract class Viewer
 	    }
 	}
 	else if (entity instanceof ListViewable)
+
 	    return ListViewer.getHtmlCode((ListViewable) entity);
 
 	throw new ViewerException("Couldn't find proper viewer for entity");
+    }
+
+    /**
+     * Gets the html code for the header ***(id="header" -> dans css pour mise
+     * en page)
+     * 
+     * @return html
+     */
+    public static String getHeader()
+    {
+	String header = "";
+	header += "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
+		+ "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" >"
+		+ "<head><title>NewtonERP"
+		+ "nom de module ici"
+		+ "</title>"
+		+ "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\" />";
+
+	// css******************************************************************
+	header += "<style type=\"text/css\">"
+		+ "body	/*mise en page pour tout le corp*/{width: 1000px;margin: auto;margin-top: 5px;margin-bottom: 5px;background-color: #ecf1f5;}"
+		+ "#header{width: 1000px;height: 75px;background-color:#d3d8e1;color:black;font-family: Arial, \"Arial Black\", \"Times New Roman\", Times, serif;border: 1px solid black;margin-bottom: 5px;}"
+		+ "#menu{float: left; /* Le menu flottant à gauche */width: 120px;}"
+		+ ".element_menu/*sous-menu*/{background-color: #d3d8e1;border: 1px solid black;margin-bottom: 10px;}"
+		+ ".element_menu h3 /* Tous les titres de sous-menus */{color: black;font-family: Arial, \"Arial Black\", \"Times New Roman\", Times, serif;text-align: center;}"
+		+ ".element_menu ul /* Listes à puces de sous-menu */{padding: 0px;padding-left: 20px;margin: 0px;margin-bottom: 5px;}"
+		+ ".element_menu a /* Tous les liens se trouvant dans un menu */{color: #0081d7;}"
+		+ ".element_menu a:hover {color: red;}"
+		+ "#body /*mise en page du corp principal à droite du menu principal*/{margin-left: 130px; /*Une marge à gauche pour pousser le corps, afin qu'il ne passe plus sous le menu*/margin-bottom: 10px;padding: 5px;color: black;background-color: #d3d8e1;border: 1px solid black;}"
+		+ "#footer{text-align: center;color: black;background-color: #d3d8e1;border: 1px solid black;}</style>";
+	// *********************************************************************
+
+	header += "</head><body><div id=\"header\">h1>NewtonERP</h1></div>";
+
+	return header;
+    }
+
+    /**
+     * Gets the html code for left menu (css pour mise en page)
+     * 
+     * @return html
+     */
+    public static String getLeftMenu()
+    {
+	Hashtable<String, String> mod = ListModule.getAllModules();
+	Iterator<String> keys = mod.keySet().iterator();
+
+	String menuModule = "";
+
+	String menu = "<div id =\"menu\"><!-- Cadre englobant tous les sous-menus -->";
+	menuModule += "<div class=\"element_menu\"> <!-- Cadre correspondant à un sous-menu -->"
+		+ "<h3>Modules</h3> <!-- Titre du sous-menu -->" + "<ul>";
+	String modName = "";
+
+	// pour l'instant un seul sous-menu, autres sous-menus à déterminer
+	while (keys.hasNext())
+	{
+	    modName = keys.next();
+	    menuModule += "<li><a href=\"" + mod.get(modName) + "\">" + modName
+		    + "</a></li>";
+	}
+	menuModule += "</ul></div>";// ferme liste et menuModule
+
+	return (menu + menuModule + "</div>");
+    }
+
+    /**
+     * Gets the html code for the footer ***(id="footer" -> dans css pour mise
+     * en page)
+     * 
+     * @return html
+     */
+    public static String getFooter()
+    {
+	String footer = "<div id=\"footer\"><p>Copyright \"G.Lacasse, J.Cloutier, J.Hallé, P.Lemay, G.Thérien\" 2009, tous droits réservés</p>/div></body></html>";
+	return footer;
     }
 }
