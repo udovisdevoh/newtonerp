dafc10a4894a79a2060f739cc4289e7c59d8ac4a
jonathan.hallee <jonathan.hallee@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-21 18:22:31 +0000

    suppression de commentaires

--- trunk/NewtonERP/src/newtonERP/module/AbstractEntity.java
+++ trunk/NewtonERP/src/newtonERP/module/AbstractEntity.java
@@ -13,10 +13,6 @@ public class AbstractEntity
 {
     private Fields fields;
 
-    // todo:étant donnée la présence de ormEntity est-ce que seulement les
-    // ormEntity devrai comporté des field ou bien tout les entity peu importe??
-    // -> r3hallejo dit : A mon avis seulement les ormEntity.... a rediscuter
-
     /**
      * construit une entity ne comportant aucun champ
      */
