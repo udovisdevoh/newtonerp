3498d93d219660852d9324bbd808826ab72609b8
deadcosmonaut <deadcosmonaut@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-22 22:52:17 +0000

    ajout du module cost"u"merVendor

---
+++ trunk/NewtonERP/src/modules/costumerVendor/CostumerVendor.java
@@ -0,0 +1,18 @@
+package modules.costumerVendor;
+
+import newtonERP.module.Module;
+
+/**
+ * @author r3lemaypa Guillaume cloutierJo
+ * 
+ */
+public class CostumerVendor extends Module
+{
+    /**
+     * constructeur
+     */
+    public CostumerVendor()
+    {
+	super();
+    }
+}
