368cd6857c0bf541d697e381316d723aa0fea50a
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-25 17:59:12 +0000

    GetRightList: fonctionnel! Grâce à notre super architechture, ça a pris 30 secondes à implémenter

---
+++ NewtonERP/src/modules/userRightModule/actions/GetRightList.java
@@ -0,0 +1,47 @@
+package modules.userRightModule.actions;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+import modules.userRightModule.UserRightModule;
+import modules.userRightModule.entityDefinitions.Right;
+import newtonERP.module.AbstractAction;
+import newtonERP.module.AbstractEntity;
+import newtonERP.module.AbstractOrmEntity;
+import newtonERP.module.EntityList;
+import newtonERP.orm.Orm;
+import newtonERP.orm.exceptions.OrmException;
+
+public class GetRightList extends AbstractAction
+{
+
+    @Override
+    protected AbstractEntity doAction(AbstractEntity entity,
+	    Hashtable<String, String> parameters) throws Exception
+    {
+	Vector<AbstractOrmEntity> rightVectorFromOrm;
+
+	try
+	{
+	    rightVectorFromOrm = Orm.select(new Right(), null);
+
+	} catch (OrmException e)
+	{
+	    rightVectorFromOrm = new Vector<AbstractOrmEntity>();
+	    e.printStackTrace();
+	}
+
+	EntityList rightList = new EntityList();
+
+	for (AbstractOrmEntity right : rightVectorFromOrm)
+	    rightList.addEntity((right));
+
+	// TODO: Problème: c'est très bizare de devoir recréer un objet de
+	// module pour spécifier quel module sera responsable des actions -
+	// Guillaume
+	rightList.setCurrentModule(new UserRightModule());
+
+	return rightList;
+    }
+
+}
