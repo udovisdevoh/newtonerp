90b54999a6b1238e0e5e0879639f8fb707122765
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-26 21:41:30 +0000

    Login form: affiché correctement mais comportement pas encore correct. Désolé pour le commit multiple, mon projet est temporairement corrompu.

--- trunk/NewtonERP/src/modules/userRightModule/actions/Login.java
+++ trunk/NewtonERP/src/modules/userRightModule/actions/Login.java
@@ -1,10 +1,10 @@
 package modules.userRightModule.actions;
 
 import java.util.Hashtable;
+import java.util.Vector;
 
 import modules.home.Home;
 import modules.home.actions.GetHome;
-import modules.userRightModule.UserRightModule;
 import modules.userRightModule.entityDefinitions.User;
 import newtonERP.Authentication;
 import newtonERP.module.AbstractAction;
@@ -12,6 +12,7 @@ import newtonERP.module.AbstractEntity;
 import newtonERP.module.AbstractOrmEntity;
 import newtonERP.module.field.Field;
 import newtonERP.module.generalEntity.ForwardEntity;
+import newtonERP.module.generalEntity.LoginForm;
 import newtonERP.serveur.Servlet;
 
 /**
@@ -32,7 +33,6 @@ public class Login extends AbstractAction
     protected AbstractEntity doAction(AbstractEntity entity,
 	    Hashtable<String, String> parameters) throws Exception
     {
-
 	if (parameters.containsKey("submit"))
 	{
 	    for (Field field : entity.getFields().getFields())
@@ -40,8 +40,11 @@ public class Login extends AbstractAction
 		field.setOperator("="); // TODO: on peu tu mettre l'operateur
 		// par defaut a = ???
 	    }
-	    if (((AbstractOrmEntity) entity).get((AbstractOrmEntity) entity)
-		    .size() > 0)
+
+	    Vector<AbstractOrmEntity> userList = ((AbstractOrmEntity) (entity))
+		    .get((AbstractOrmEntity) (entity));
+
+	    if (userList.size() > 0 && userList.get(0).getData("name") != null)
 	    {
 		Authentication.setCurrentUserName(entity.getDataString("name"));
 		return new ForwardEntity(Servlet.makeLink(new Home(),
@@ -49,10 +52,17 @@ public class Login extends AbstractAction
 	    }
 	}
 
-	User retEntity = new User();
-	retEntity.setCurrentAction(this);
-	retEntity.setCurrentModule(new UserRightModule());
+	/*
+	 * User retEntity = new User(); retEntity.setCurrentAction(this);
+	 * retEntity.setCurrentModule(new UserRightModule());
+	 * 
+	 * return retEntity;
+	 */
 
-	return retEntity;
+	String currentLoginName = parameters.get("name");
+	if (currentLoginName == null)
+	    currentLoginName = "";
+
+	return new LoginForm(currentLoginName);
     }
 }
