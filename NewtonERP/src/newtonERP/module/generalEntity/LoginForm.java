f67671cc6849c5c8c970e93d4e5f163c00a1b151
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-26 21:42:05 +0000

    Login form: affiché correctement mais comportement pas encore correct. Désolé pour le commit multiple, mon projet est temporairement corrompu.

---
+++ NewtonERP/src/newtonERP/module/generalEntity/LoginForm.java
@@ -0,0 +1,89 @@
+package newtonERP.module.generalEntity;
+
+import java.util.Hashtable;
+
+import modules.userRightModule.UserRightModule;
+import modules.userRightModule.actions.Login;
+import newtonERP.module.AbstractAction;
+import newtonERP.module.AbstractEntity;
+import newtonERP.module.Module;
+import newtonERP.orm.exceptions.OrmException;
+import newtonERP.viewers.viewables.PromptViewable;
+
+public class LoginForm extends AbstractEntity implements PromptViewable
+{
+    private Module defaultModule;
+    private Hashtable<String, String> inputList = new Hashtable<String, String>();
+    private String currentSelectedUser;
+
+    public LoginForm(String currentSelectedUser) throws Exception
+    {
+	this.currentSelectedUser = currentSelectedUser;
+	defaultModule = new UserRightModule();
+    }
+
+    @Override
+    public String getButtonCaption()
+    {
+	return "Entrer";
+    }
+
+    @Override
+    public AbstractAction getCurrentAction()
+    {
+	return new Login();
+    }
+
+    @Override
+    public Module getCurrentModule()
+    {
+	return defaultModule;
+    }
+
+    @Override
+    public Hashtable<String, FlagPool> getFlagPoolList()
+    {
+	// TODO Auto-generated method stub
+	return new Hashtable<String, FlagPool>();
+    }
+
+    @Override
+    public Hashtable<String, String> getInputList() throws OrmException
+    {
+	inputList.put("name", currentSelectedUser);
+	inputList.put("password", "");
+	return inputList;
+    }
+
+    @Override
+    public String getLabelName(String inputName)
+    {
+	if (inputName == "name")
+	    return "Utilisateur";
+	else if (inputName == "password")
+	    return "Mot de passe";
+	else
+	    return "nom de champ invalide";
+    }
+
+    @Override
+    public String getPromptMessage()
+    {
+	return "Identification";
+    }
+
+    @Override
+    public boolean isFieldHidden(String fieldName)
+    {
+	if (fieldName.equals("password"))
+	    return true;
+	return false;
+    }
+
+    @Override
+    public ListOfValue tryMatchListOfValue(String fieldKeyName)
+    {
+	return null;
+    }
+
+}
