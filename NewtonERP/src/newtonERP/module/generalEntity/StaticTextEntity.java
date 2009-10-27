e1002c2250abb4af995d7b00c9d7dad55b1b8c96
jonathan.hallee <jonathan.hallee@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-10-27 23:49:53 +0000

    ajout de commentaires merci de complété

--- NewtonERP/src/newtonERP/module/generalEntity/StaticTextEntity.java
+++ NewtonERP/src/newtonERP/module/generalEntity/StaticTextEntity.java
@@ -3,11 +3,19 @@ package newtonERP.module.generalEntity;
 import newtonERP.module.AbstractEntity;
 import newtonERP.viewers.viewables.StaticTextViewable;
 
+/**
+ * @author Guillaume Lacasse
+ * 
+ *         Represents a static text entity
+ */
 public class StaticTextEntity extends AbstractEntity implements
 	StaticTextViewable
 {
     private String text;
 
+    /**
+     * @param text the text of the entity
+     */
     public StaticTextEntity(String text)
     {
 	this.text = text;
