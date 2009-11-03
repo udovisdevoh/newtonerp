bd5096aa6cba8811d1e7197e011741611adf4aa6
jonathan.hallee <jonathan.hallee@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-03 00:53:29 +0000

    cleanup

--- trunk/NewtonERP/src/modules/materialResourcesManagement/MaterialResourcesManagement.java
+++ trunk/NewtonERP/src/modules/materialResourcesManagement/MaterialResourcesManagement.java
@@ -31,30 +31,4 @@ public class MaterialResourcesManagement extends Module
 		new Product()));
 	setVisibleName("Ressources matérielles");
     }
-
-    /*
-     * public void initDB() throws Exception { // insert de une adresse Address
-     * adresse = new Address(); adresse.setData("city", "Montréal");
-     * adresse.setData("streetName", "Ontario"); adresse.setData("streetNumber",
-     * 255); adresse.setData("postalCode", "HOH OHO");
-     * adresse.setData("telephoneNumber", "514-343-5678");
-     * 
-     * Orm.insert(adresse);
-     * 
-     * // retrouve le addressID int addressID = (Integer) ((Address)
-     * Orm.select(adresse).get(0)) .getData(adresse.getPrimaryKeyName());
-     * 
-     * Warehouse warehouse = new Warehouse(); warehouse.setData("warehouseName",
-     * "Arb_Montréal"); warehouse.setData("addressID", addressID);
-     * 
-     * Orm.insert(warehouse);
-     * 
-     * for (int i = 0; i < 10; i++) { Product product = new Product();
-     * product.setData("code", "A40898B6" + i); product.setData("name",
-     * "Tuile plancher 12 * 12");
-     * 
-     * Orm.insert(product); }
-     * 
-     * }
-     */
 }
