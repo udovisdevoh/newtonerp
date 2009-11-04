3facc1c31b667e96f13d9f550aad7329ff0d0ee9
deadcosmonaut <deadcosmonaut@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-04 23:45:21 +0000

    fournisseurs de services

---
+++ NewtonERP/src/modules/finances/entityDefinitions/ServiceProvider.java
@@ -0,0 +1,48 @@
+package modules.finances.entityDefinitions;
+
+import java.util.Vector;
+
+import newtonERP.module.AbstractOrmEntity;
+import newtonERP.module.field.Field;
+import newtonERP.module.field.FieldInt;
+import newtonERP.module.field.FieldString;
+import newtonERP.module.field.Fields;
+import newtonERP.viewers.viewables.PromptViewable;
+
+/**
+ * Entité ServiceProvider du module finances: représente les coordonnées des
+ * fournisseurs de services (internet, électricité...)
+ * 
+ * Pour avoir les info à l'extérieur de ServiceProviderAccount
+ * 
+ * @author Pascal Lemay
+ */
+
+public class ServiceProvider extends AbstractOrmEntity implements
+	PromptViewable
+{
+    /**
+     * @throws Exception if creation fails
+     */
+    public ServiceProvider() throws Exception
+    {
+	super();
+
+	setVisibleName("Fournisseurs de services");
+    }
+
+    @Override
+    public Fields initFields() throws Exception
+    {
+	Vector<Field> fieldsInit = new Vector<Field>();
+	fieldsInit.add(new FieldInt("Numéro", getPrimaryKeyName()));
+	fieldsInit.add(new FieldString("Nom", "name"));
+	fieldsInit.add(new FieldInt("Numéro civique", "civicNumber"));
+	fieldsInit.add(new FieldString("Rue", "streetName"));
+	fieldsInit.add(new FieldString("Ville", "city"));
+	fieldsInit.add(new FieldString("Code postal", "zipCode"));
+
+	return new Fields(fieldsInit);
+    }
+
+}
