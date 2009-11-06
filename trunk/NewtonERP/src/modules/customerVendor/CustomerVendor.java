373c8573aedd7ad05513c2e96e397a8671fb7ebf
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-06 15:30:57 +0000

    Client (fournisseur ou client) a maintenant un accessor vers la classe Adresse

--- trunk/NewtonERP/src/modules/customerVendor/CustomerVendor.java
+++ trunk/NewtonERP/src/modules/customerVendor/CustomerVendor.java
@@ -33,30 +33,26 @@ public class CustomerVendor extends Module
     public void initDB() throws Exception
     {
 	super.initDB();
-
-	Customer customer3 = new Customer();
-	customer3.setData("name", "Aucun");
-	customer3.setData("phone", "Aucun");
-	customer3.setData("address", "Aucun");
-	customer3.newE();
-
-	Customer customer = new Customer();
-	customer.setData("name", "Bombardier");
-	customer.setData("phone", "514-345-6575");
-	customer.setData("address", "1084 Sherbrooke, Montreal");
-	customer.newE();
-
-	Customer customer1 = new Customer();
-	customer1.setData("name", "Hydro-Québec");
-	customer1.setData("phone", "514-780-4568");
-	customer1.setData("address", "458 Sainte-Catherine, Montreal");
-	customer1.newE();
-
-	Customer customer2 = new Customer();
-	customer2.setData("name", "Arborite");
-	customer2.setData("phone", "514-266-2710");
-	customer2.setData("address", "24 Dollard, Lasalle");
-	customer2.newE();
+	/*
+	 * Customer customer3 = new Customer(); customer3.setData("name",
+	 * "Nous"); customer3.setData("phone", "555-5555");
+	 * customer3.setData("address", "666 St-Lucifer"); customer3.newE();
+	 * 
+	 * Customer customer = new Customer(); customer.setData("name",
+	 * "Bombardier"); customer.setData("phone", "514-345-6575");
+	 * customer.setData("address", "1084 Sherbrooke, Montreal");
+	 * customer.newE();
+	 * 
+	 * Customer customer1 = new Customer(); customer1.setData("name",
+	 * "Hydro-Québec"); customer1.setData("phone", "514-780-4568");
+	 * customer1.setData("address", "458 Sainte-Catherine, Montreal");
+	 * customer1.newE();
+	 * 
+	 * Customer customer2 = new Customer(); customer2.setData("name",
+	 * "Arborite"); customer2.setData("phone", "514-266-2710");
+	 * customer2.setData("address", "24 Dollard, Lasalle");
+	 * customer2.newE();
+	 */
 
     }
 }
