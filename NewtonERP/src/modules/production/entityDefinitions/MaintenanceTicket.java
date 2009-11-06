fbb6de60670e952aeb34daa213dc2f6926dbffe1
jonathan.hallee <jonathan.hallee@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-06 02:06:45 +0000

    debut d'un module de gestion de l'entretien des machines. JO trouve pas ca bizarre si le boss a un ticket de maintenance ya pas d'employé au département maintenance actuellement alors il s'en occupe lui meme!

---
+++ NewtonERP/src/modules/production/entityDefinitions/MaintenanceTicket.java
@@ -0,0 +1,52 @@
+package modules.production.entityDefinitions;
+
+import java.util.Vector;
+
+import modules.humanResources.entityDefinitions.Employee;
+import newtonERP.module.AbstractOrmEntity;
+import newtonERP.module.field.Field;
+import newtonERP.module.field.FieldDateTime;
+import newtonERP.module.field.FieldInt;
+import newtonERP.module.field.FieldString;
+import newtonERP.module.field.Fields;
+import newtonERP.orm.associations.AccessorManager;
+import newtonERP.viewers.viewables.PromptViewable;
+
+/**
+ * A new maintenance work order to do because of a broken machine
+ * 
+ * @author r3hallejo
+ */
+public class MaintenanceTicket extends AbstractOrmEntity implements
+	PromptViewable
+{
+
+    /**
+     * Default constructor
+     * 
+     * @throws Exception a general exception
+     */
+    public MaintenanceTicket() throws Exception
+    {
+	super();
+	setVisibleName("Ticket de maintenance");
+	AccessorManager.addAccessor(this, new Employee());
+	AccessorManager.addAccessor(this, new StatusType());
+    }
+
+    @Override
+    public Fields initFields() throws Exception
+    {
+	Vector<Field> fieldsInit = new Vector<Field>();
+	fieldsInit.add(new FieldInt("Numéro du ticket", getPrimaryKeyName()));
+	fieldsInit.add(new FieldString("Problème rencontré", "problemType"));
+	fieldsInit.add(new FieldInt("Assigné à ", new Employee()
+		.getForeignKeyName()));
+	fieldsInit.add(new FieldDateTime("Date de début", "startDate"));
+	fieldsInit.add(new FieldDateTime("Date de fin", "endDate"));
+	fieldsInit.add(new FieldInt("Status", new StatusType()
+		.getForeignKeyName()));
+	fieldsInit.add(new FieldString("Commentaire", "comment"));
+	return new Fields(fieldsInit);
+    }
+}
