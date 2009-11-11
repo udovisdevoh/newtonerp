b1f22f20f5761767ee58ec7ad57700263d120045
saluthi <saluthi@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-11 19:21:10 +0000

    le module du marketing merci

---
+++ trunk/NewtonERP/src/modules/marketing/entityDefinitions/Promotion.java
@@ -0,0 +1,45 @@
+package modules.marketing.entityDefinitions;
+
+import java.util.Vector;
+
+import newtonERP.module.AbstractOrmEntity;
+import newtonERP.module.field.Field;
+import newtonERP.module.field.FieldDate;
+import newtonERP.module.field.FieldInt;
+import newtonERP.module.field.FieldString;
+import newtonERP.module.field.Fields;
+import newtonERP.viewers.viewables.PromptViewable;
+
+/**
+ * Entité d'une promotion
+ * @author Gabriel
+ * 
+ */
+public class Promotion extends AbstractOrmEntity implements PromptViewable
+{
+    /**
+     * @throws Exception si création fail
+     */
+    public Promotion() throws Exception
+    {
+	super();
+	addCurrencyFormat("budget");
+	setVisibleName("Promotion");
+    }
+
+    @Override
+    public Fields initFields() throws Exception
+    {
+
+	Vector<Field> fieldsInit = new Vector<Field>();
+	fieldsInit.add(new FieldInt("Numéro", getPrimaryKeyName()));
+	fieldsInit.add(new FieldString("Nom De La Promotion", "promotionname"));
+	fieldsInit.add(new FieldDate("Date du début de la promotion",
+		"startDate"));
+	fieldsInit.add(new FieldDate("Date de la fin de la promotion",
+		"endingDate"));
+	fieldsInit.add(new FieldInt("Budjet accordé", "budget"));
+	fieldsInit.add(new FieldString("Notes de la modalité", "modality"));
+	return new Fields(fieldsInit);
+    }
+}
