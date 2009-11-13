08d214699c16ab5c3217f8ff8687d7e1c75cda59
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-13 22:30:42 +0000

    Task module

--- NewtonERP/src/modules/taskModule/TaskModule.java
+++ NewtonERP/src/modules/taskModule/TaskModule.java
@@ -42,6 +42,8 @@ public class TaskModule extends Module
     {
 	super.initDB();
 
+	// Les entitées suivantes sont là pour avoir des données par default
+	// qu'on doit garder
 	SearchCriteriaOperator searchCriteriaOperator;
 
 	searchCriteriaOperator = new SearchCriteriaOperator();
@@ -79,5 +81,36 @@ public class TaskModule extends Module
 	searchCriteriaOperator = new SearchCriteriaOperator();
 	searchCriteriaOperator.setData("name", "like");
 	searchCriteriaOperator.newE();
+
+	// Contrairement aux précédentes, les entitées suivantes sont là juste
+	// pour tester
+	Parameter parameter = new Parameter();
+	parameter.setData("key", "message");
+	parameter.setData("value", "vous etes vire mon estie");
+	parameter.newE();
+
+	SearchCriteria searchCriteria = new SearchCriteria();
+	searchCriteria.setData("key", "salaire");
+	searchCriteria.setData(new SearchEntity().getForeignKeyName(), 1);
+	searchCriteria.setData(
+		new SearchCriteriaOperator().getForeignKeyName(), 5);
+	searchCriteria.setData("value", "33400");
+	searchCriteria.newE();
+
+	SearchEntity searchEntity = new SearchEntity();
+	searchEntity.setData("name", "Employe qui a un salaire poche");
+	searchEntity.setData("entitySystemName", "Employee");
+	searchEntity.newE();
+
+	Effect effect = new Effect();
+	effect.setData("name", "On congedie le dude");
+	effect.setData(new SearchEntity().getForeignKeyName(), 1);
+	effect.setData("actionSystemName", "Delete");
+	effect.newE();
+
+	Specification specification = new Specification();
+	specification.setData("name", "Si employe en retard");
+	specification.setData(new SearchEntity().getForeignKeyName(), 1);
+	specification.newE();
     }
 }
