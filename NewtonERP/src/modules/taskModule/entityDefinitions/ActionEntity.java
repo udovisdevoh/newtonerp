669fb0ade2478f03f50d15842c8c4b648bbaaad9
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-14 17:34:31 +0000

    Task Module

--- NewtonERP/src/modules/taskModule/entityDefinitions/ActionEntity.java
+++ NewtonERP/src/modules/taskModule/entityDefinitions/ActionEntity.java
@@ -3,6 +3,7 @@ package modules.taskModule.entityDefinitions;
 import java.util.Vector;
 
 import newtonERP.module.AbstractOrmEntity;
+import newtonERP.orm.associations.AccessorManager;
 import newtonERP.orm.field.Field;
 import newtonERP.orm.field.FieldInt;
 import newtonERP.orm.field.FieldString;
@@ -22,8 +23,9 @@ public class ActionEntity extends AbstractOrmEntity implements PromptViewable
     {
 	super();
 	setVisibleName("Action");
-	addNaturalKey("moduleName");
+	addNaturalKey(new ModuleEntity().getForeignKeyName());
 	addNaturalKey("systemName");
+	AccessorManager.addAccessor(this, new ModuleEntity());
     }
 
     @Override
@@ -32,8 +34,8 @@ public class ActionEntity extends AbstractOrmEntity implements PromptViewable
 	Vector<Field> fieldList = new Vector<Field>();
 	fieldList.add(new FieldInt("Numéro", getPrimaryKeyName()));
 	fieldList.add(new FieldString("Nom système", "systemName"));
-	fieldList.add(new FieldString("Module", "moduleName"));
+	fieldList.add(new FieldInt("Module", new ModuleEntity()
+		.getForeignKeyName()));
 	return new Fields(fieldList);
     }
-
 }
