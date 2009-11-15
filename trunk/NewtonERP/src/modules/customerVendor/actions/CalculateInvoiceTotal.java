380f51b817e564059cb3404e642afa09be902924
jonathan.hallee <jonathan.hallee@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-15 03:00:10 +0000

    On a des taxes qui se calcule. Peut-importe si le user clique 20 fois sur calculer je refait le calcul au complet a chaque fois...

--- trunk/NewtonERP/src/modules/customerVendor/actions/CalculateInvoiceTotal.java
+++ trunk/NewtonERP/src/modules/customerVendor/actions/CalculateInvoiceTotal.java
@@ -5,6 +5,7 @@ import java.util.Vector;
 
 import modules.customerVendor.entityDefinitions.Invoice;
 import modules.customerVendor.entityDefinitions.InvoiceLine;
+import modules.customerVendor.entityDefinitions.Tax;
 import newtonERP.module.AbstractAction;
 import newtonERP.module.AbstractEntity;
 import newtonERP.module.AbstractOrmEntity;
@@ -12,8 +13,6 @@ import newtonERP.orm.Orm;
 
 /**
  * Will be called by tasks. To calculate the total of the invoice
- * 
- * TODO : Add the taxes (from another action?)
  * 
  * @author r3hallejo
  */
@@ -35,6 +34,10 @@ public class CalculateInvoiceTotal extends AbstractAction
     {
 	Invoice actionInvoice = (Invoice) entity;
 	Double totalInvoice = (Double) entity.getData("total");
+	Double taxTotal = (Double) entity.getData("taxTotal");
+
+	taxTotal = 0.0;
+	totalInvoice = 0.0;
 
 	// We get the invoiceLines associated
 	Vector<AbstractOrmEntity> invoiceLines = actionInvoice
@@ -42,14 +45,28 @@ public class CalculateInvoiceTotal extends AbstractAction
 
 	for (AbstractOrmEntity line : invoiceLines)
 	{
-	    if (totalInvoice == null)
-		totalInvoice = 0.0;
-
 	    totalInvoice += ((Double) ((InvoiceLine) line).getData("unitPrice") * (Integer) line
 		    .getData("quantity"));
 	}
 
 	actionInvoice.setData("total", totalInvoice);
+
+	Vector<AbstractOrmEntity> invoiceTaxLines = actionInvoice
+		.getPluralAccessor("InvoiceTaxLine");
+
+	// For each tax
+	for (AbstractOrmEntity tax : invoiceTaxLines)
+	{
+	    Tax taxe = new Tax();
+	    taxe.setData(new Tax().getPrimaryKeyName(), tax
+		    .getPrimaryKeyValue());
+
+	    Tax myTax = (Tax) Orm.selectUnique(taxe);
+
+	    taxTotal += totalInvoice * ((Double) myTax.getData("value") / 100);
+	}
+
+	actionInvoice.setData("taxTotal", taxTotal);
 
 	Vector<String> searchCriterias = new Vector<String>();
 	searchCriterias.add(actionInvoice.getPrimaryKeyName() + "='"
