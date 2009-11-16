323000b2f2ccac72f408732e37f61abc0d898c7f
jonathan.hallee <jonathan.hallee@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-16 00:04:27 +0000

    Ajout d'un field boolean sur la facture pour déterminé si c'est pour un client ou pas a la demande de pascal (je lui en ait parlé je suis d'accord je vais moi meme avoir besoin de ca pour plus tard).
    Ajout d'un invoice status (Nouveau, Payé, Fermer etc...)

---
+++ trunk/NewtonERP/src/modules/customerVendor/entityDefinitions/InvoiceStatus.java
@@ -0,0 +1,40 @@
+package modules.customerVendor.entityDefinitions;
+
+import java.util.Vector;
+
+import newtonERP.module.AbstractOrmEntity;
+import newtonERP.orm.field.Field;
+import newtonERP.orm.field.FieldInt;
+import newtonERP.orm.field.FieldString;
+import newtonERP.orm.field.Fields;
+import newtonERP.viewers.viewables.PromptViewable;
+
+/**
+ * Represents an invoice status
+ * 
+ * @author r3hallejo
+ */
+public class InvoiceStatus extends AbstractOrmEntity implements PromptViewable
+{
+
+    /**
+     * Default constructor
+     * 
+     * @throws Exception a general exception
+     */
+    public InvoiceStatus() throws Exception
+    {
+	super();
+	setVisibleName("Status");
+    }
+
+    @Override
+    public Fields initFields() throws Exception
+    {
+	Vector<Field> fieldList = new Vector<Field>();
+	fieldList.add(new FieldInt("Numero", getPrimaryKeyName()));
+	fieldList.add(new FieldString("Status", "status"));
+	return new Fields(fieldList);
+    }
+
+}
