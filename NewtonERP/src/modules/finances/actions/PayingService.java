7d9ab0213979ae55024718f22c937de059e50370
deadcosmonaut <deadcosmonaut@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-18 19:25:16 +0000

    actions de gestion...

--- NewtonERP/src/modules/finances/actions/PayingService.java
+++ NewtonERP/src/modules/finances/actions/PayingService.java
@@ -39,6 +39,8 @@ public class PayingService extends AbstractAction
 	 * account.getPrimaryKeyValue() + "'"); Orm.update(account,
 	 * searchCriterias);
 	 */
+
+	new DebitFromBankAccount().doAction(null, null);// test
 	account.save();
 	return account.getList();
 
