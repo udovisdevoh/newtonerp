5397abc9ab30d9bfd3fd4c37dc96df0301ed5931
deadcosmonaut <deadcosmonaut@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-18 21:17:49 +0000

    début de test avec action DebitFromBankAccount à partir de PayingService et PayingSupplier

--- NewtonERP/src/modules/finances/actions/PayingService.java
+++ NewtonERP/src/modules/finances/actions/PayingService.java
@@ -2,15 +2,19 @@ package modules.finances.actions;
 
 import java.util.GregorianCalendar;
 import java.util.Hashtable;
+import java.util.Vector;
 
+import modules.finances.entityDefinitions.BankAccount;
 import modules.finances.entityDefinitions.ServiceProviderAccount;
 import modules.finances.entityDefinitions.StateType;
 import newtonERP.module.AbstractAction;
 import newtonERP.module.AbstractEntity;
+import newtonERP.module.AbstractOrmEntity;
+import newtonERP.orm.Orm;
 
 /**
  * Action PayingService: représente l'action de payer le compte correspondant.
- * ps pas terminer
+ * ps pas terminé
  * @author Pascal Lemay
  */
 public class PayingService extends AbstractAction
@@ -39,7 +43,11 @@ public class PayingService extends AbstractAction
 	 * searchCriterias);
 	 */
 
-	new DebitFromBankAccount().doAction(null, null);// test
+	BankAccount searchEntity = new BankAccount();
+	searchEntity.setData("folio", "2148");// tempo
+	Vector<AbstractOrmEntity> bankAccount = Orm.select(searchEntity);
+	new DebitFromBankAccount().doAction(bankAccount.get(0), null);
+
 	account.save();
 	return account.getList();
 
