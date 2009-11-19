909ac6256da5596f8e16f129993791d0aa671672
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-19 13:36:34 +0000

    Module Système: Ajouté entité d'accesseur

---
+++ trunk/NewtonERP/src/modules/taskModule/entityDefinitions/AccessorEntity.java
@@ -0,0 +1,40 @@
+package modules.taskModule.entityDefinitions;
+
+import java.util.Vector;
+
+import newtonERP.module.AbstractOrmEntity;
+import newtonERP.orm.associations.AccessorManager;
+import newtonERP.orm.field.Field;
+import newtonERP.orm.field.FieldInt;
+import newtonERP.orm.field.Fields;
+
+/**
+ * Entité qui représente un accesseur
+ * @author Guillaume Lacasse
+ */
+public class AccessorEntity extends AbstractOrmEntity
+{
+    /**
+     * @throws Exception si création fail
+     */
+    public AccessorEntity() throws Exception
+    {
+	super();
+	setVisibleName("Accesseur");
+	AccessorManager.addAccessor(this, new EntityEntity());
+	AccessorManager.addAccessor(this, new SearchEntity());
+    }
+
+    @Override
+    public Fields initFields() throws Exception
+    {
+	Vector<Field> fieldList = new Vector<Field>();
+	fieldList.add(new FieldInt("Numéro", getPrimaryKeyName()));
+	fieldList.add(new FieldInt("Entité parent", new EntityEntity()
+		.getForeignKeyName()));
+	fieldList.add(new FieldInt("Entité étrangère", new SearchEntity()
+		.getForeignKeyName()));
+	return new Fields(fieldList);
+    }
+
+}
