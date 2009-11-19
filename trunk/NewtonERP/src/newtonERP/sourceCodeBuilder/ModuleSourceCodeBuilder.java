35860d8c4be6c9cdddb15f9b293c9ba174c309f9
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-19 17:11:34 +0000

    Il est maintenant possible de voir le code source généré d'un module softcodé. Reste à en faire de même pour les entités et les actions

--- trunk/NewtonERP/src/newtonERP/sourceCodeBuilder/ModuleSourceCodeBuilder.java
+++ trunk/NewtonERP/src/newtonERP/sourceCodeBuilder/ModuleSourceCodeBuilder.java
@@ -1,6 +1,7 @@
 package newtonERP.sourceCodeBuilder;
 
 import modules.taskModule.entityDefinitions.ModuleEntity;
+import newtonERP.orm.Orm;
 
 /**
  * Cette classe sert à construire le code source de module
@@ -11,14 +12,58 @@ public class ModuleSourceCodeBuilder
     /**
      * @param moduleEntity entité du module
      * @return code source
+     * @throws Exception
      */
-    public static String build(ModuleEntity moduleEntity)
+    public static String build(ModuleEntity moduleEntity) throws Exception
     {
+	moduleEntity = (ModuleEntity) Orm.selectUnique(moduleEntity);
+
 	String sourceCode = "";
 
-	sourceCode += "//TEST1\n";
-	sourceCode += "//TEST2\n";
+	sourceCode += "package modules." + getPackageName(moduleEntity) + ";\n";
+	sourceCode += "\n";
+	sourceCode += "/**\n";
+	sourceCode += " * " + getVisibleName(moduleEntity) + "\n";
+	sourceCode += " * @author NewtonERP code generator\n";
+	sourceCode += " */\n";
+
+	sourceCode += "public class " + getSystemName(moduleEntity)
+		+ " extends Module\n";
+	sourceCode += "{\n";
+	sourceCode += "    /**\n";
+	sourceCode += "     * constructor\n";
+	sourceCode += "     * @throws Exception remonte\n";
+	sourceCode += "     */\n";
+	sourceCode += "    public " + getSystemName(moduleEntity)
+		+ "() throws Exception\n";
+	sourceCode += "    {\n";
+	sourceCode += "        super();\n";
+	sourceCode += "        setVisibleName(\""
+		+ getVisibleName(moduleEntity) + "\");\n";
+	sourceCode += "    }\n";
+	sourceCode += "\n";
+	sourceCode += "    public void initDB() throws Exception\n";
+	sourceCode += "    {\n";
+	sourceCode += "        super.initDB();\n";
+	sourceCode += "    }\n";
+	sourceCode += "}\n";
 
 	return sourceCode;
+    }
+
+    private static String getVisibleName(ModuleEntity moduleEntity)
+    {
+	return moduleEntity.getDataString("visibleName");
+    }
+
+    private static String getPackageName(ModuleEntity moduleEntity)
+    {
+	return getSystemName(moduleEntity).substring(0, 1).toLowerCase()
+		+ getSystemName(moduleEntity).substring(1);
+    }
+
+    private static String getSystemName(ModuleEntity moduleEntity)
+    {
+	return moduleEntity.getDataString("systemName");
     }
 }
