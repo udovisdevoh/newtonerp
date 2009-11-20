d138636f4d5dd74bb47f4fce014d2d8d6795806c
saluthi <saluthi@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-20 19:15:18 +0000

    des actions (calculateOffer GetAndCalculateAssiciatedOffer) entiter améliorer ddetailedOffer- qui est rendu LineOffer

---
+++ NewtonERP/src/modules/marketing/actions/GetAndCalculateAssociatedOffer.java
@@ -0,0 +1,38 @@
+package modules.marketing.actions;
+
+import java.util.Hashtable;
+
+import modules.marketing.entityDefinitions.LineOffer;
+import modules.marketing.entityDefinitions.Offer;
+import newtonERP.module.AbstractAction;
+import newtonERP.module.AbstractEntity;
+import newtonERP.orm.Orm;
+
+/**
+ * trouve les lignes
+ * @author Gabriel Therrien
+ * 
+ */
+public class GetAndCalculateAssociatedOffer extends AbstractAction
+{
+
+    /**
+     * 
+     */
+    public GetAndCalculateAssociatedOffer() throws Exception
+    {
+	super(new LineOffer());
+    }
+
+    public AbstractEntity doAction(AbstractEntity entity,
+	    Hashtable<String, String> parameters) throws Exception
+    {
+	Offer offre = (Offer) entity;
+	LineOffer lineOffer = (LineOffer) entity;
+	offre.setData(offre.getPrimaryKeyName(), lineOffer.getData(new Offer()
+		.getForeignKeyName()));
+	Offer returnedOffer = (Offer) Orm.selectUnique(offre);
+
+	return null;
+    }
+}
--- NewtonERP/src/modules/marketing/entityDefinitions/LineOffer.java
+++ NewtonERP/src/modules/marketing/entityDefinitions/LineOffer.java
@@ -3,26 +3,27 @@ package modules.marketing.entityDefinitions;
 import java.util.Vector;
 
 import newtonERP.module.AbstractOrmEntity;
+import newtonERP.orm.associations.AccessorManager;
 import newtonERP.orm.field.Field;
 import newtonERP.orm.field.Fields;
 import newtonERP.orm.field.Type.FieldCurrency;
 import newtonERP.orm.field.Type.FieldInt;
-import newtonERP.orm.field.Type.FieldString;
 
 /**
  * Entité d'une promotion
  * @author Gabriel
  * 
  */
-public class Offer extends AbstractOrmEntity
+public class LineOffer extends AbstractOrmEntity
 {
     /**
      * @throws Exception si création fail
      */
-    public Offer() throws Exception
+    public LineOffer() throws Exception
     {
 	super();
 	setVisibleName("Offre");
+	AccessorManager.addAccessor(this, new Offer());
     }
 
     @Override
@@ -31,9 +32,9 @@ public class Offer extends AbstractOrmEntity
 
 	Vector<Field<?>> fieldsInit = new Vector<Field<?>>();
 	fieldsInit.add(new FieldInt("Numéro", getPrimaryKeyName()));
-	fieldsInit.add(new FieldString("Nom De l'offre", "Offre"));
+	fieldsInit.add(new FieldInt("Offre", new Offer().getForeignKeyName()));
 	FieldCurrency currencyField = new FieldCurrency("Prix", "Price");
-	currencyField.setReadOnly(true);
+	// currencyField.setReadOnly(true);
 	fieldsInit.add(currencyField);
 	return new Fields(fieldsInit);
     }
