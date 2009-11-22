4ce4a896710410f4d0b236345f0986bf19743dd3
deadcosmonaut <deadcosmonaut@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-22 04:18:22 +0000

    commentaires

--- trunk/NewtonERP/src/modules/finances/actions/PayingService.java
+++ trunk/NewtonERP/src/modules/finances/actions/PayingService.java
@@ -35,17 +35,23 @@ public class PayingService extends AbstractAction
 
 	ServiceTransaction searchT = new ServiceTransaction();
 	searchT.setData(trans.getPrimaryKeyName(), trans.getPrimaryKeyValue());
+	// La transaction
 	AbstractOrmEntity transaction = Orm.selectUnique(searchT);
-
+	// Le solde
 	String bill = String.valueOf(transaction.getData("balance"));
 
 	BankAccount searchBank = new BankAccount();
 	searchBank.setData(new BankAccount().getPrimaryKeyName(), transaction
 		.getData(new BankAccount().getForeignKeyName()));
+	// Le compte de banque pour paiement
 	AbstractOrmEntity bankAccount = Orm.selectUnique(searchBank);
 
+	// Le solde comme paramètre
 	Hashtable<String, String> actionParameters = new Hashtable<String, String>();
 	actionParameters.put("bill", bill);
+
+	// Passage du compte de banque et du solde de la transaction
+	// à l'action DebitFromBankAccount()
 	AlertEntity alert = (AlertEntity) new DebitFromBankAccount().doAction(
 		bankAccount, actionParameters);
 
