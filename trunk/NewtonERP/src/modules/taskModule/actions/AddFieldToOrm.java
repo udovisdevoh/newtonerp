64c4ec902afddefc058d26715b5978b264ce2468
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-28 01:23:33 +0000

    AddFieldToOrmL Prise en charge des cas ou champ déjà dans DB

--- trunk/NewtonERP/src/modules/taskModule/actions/AddFieldToOrm.java
+++ trunk/NewtonERP/src/modules/taskModule/actions/AddFieldToOrm.java
@@ -8,6 +8,7 @@ import newtonERP.module.AbstractAction;
 import newtonERP.module.AbstractEntity;
 import newtonERP.module.AbstractOrmEntity;
 import newtonERP.orm.Orm;
+import newtonERP.orm.exceptions.OrmException;
 import newtonERP.orm.field.Field;
 import newtonERP.viewers.viewerData.BaseViewerData;
 
@@ -38,8 +39,18 @@ public class AddFieldToOrm extends AbstractAction
 	AbstractOrmEntity entity = entityEntity.getEntityDefinition();
 	Field<?> field = fieldEntity.getFieldInstance();
 
-	Orm.addColumnToTable(entity, field);
+	BaseViewerData editUI = fieldEntity.editUI(parameters);
 
-	return fieldEntity.editUI(parameters);
+	try
+	{
+	    Orm.addColumnToTable(entity, field);
+	    editUI
+		    .addNormalMessage("Le champ a été inséré dans la base de donnée");
+	} catch (OrmException e)
+	{
+	    editUI.addNormalMessage("Champ déjà dans la base de donnée");
+	}
+
+	return editUI;
     }
 }
