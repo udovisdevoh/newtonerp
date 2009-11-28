2dad1f77b81bbc0aa25cf4c95e03636d397991c3
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-28 00:45:49 +0000

    djo: je ne sais pas exactement comment ça se fait mais ça pop le message corrigé dans mon commit lorsque j'edit une task.

--- NewtonERP/src/newtonERP/orm/field/FieldValidator.java
+++ NewtonERP/src/newtonERP/orm/field/FieldValidator.java
@@ -7,7 +7,7 @@ package newtonERP.orm.field;
  */
 public abstract class FieldValidator<T>
 {
-    private String errorMessage = "une erreur inconnue c'est produit";
+    private String errorMessage = "une erreur inconnue s'est produite";
 
     protected abstract boolean valide(T value);
 
