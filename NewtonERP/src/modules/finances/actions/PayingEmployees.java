b45cbd2972e889b77bea9706e0da57a6e1f1b6f2
deadcosmonaut <deadcosmonaut@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-11-29 00:26:08 +0000

    PayableEmployee,payingEmployee,payingEmployees,

---
+++ NewtonERP/src/modules/finances/actions/PayingEmployees.java
@@ -0,0 +1,62 @@
+package modules.finances.actions;
+
+import java.util.Collection;
+import java.util.Hashtable;
+
+import modules.finances.entityDefinitions.PayableEmployee;
+import modules.humanResources.entityDefinitions.Employee;
+import newtonERP.module.AbstractAction;
+import newtonERP.module.AbstractEntity;
+import newtonERP.module.AbstractOrmEntity;
+import newtonERP.module.generalEntity.AlertEntity;
+import newtonERP.orm.Orm;
+
+/**
+ * Action PayingEmployee: représente l'action de payer les employés
+ * correspondants au ID passés en paramètres.
+ * 
+ * Pourra être callé par task également
+ * 
+ * parameters data:ID des employés que l'on veut payer
+ * 
+ * @author Pascal Lemay
+ */
+public class PayingEmployees extends AbstractAction
+{
+    /**
+     * constructeur
+     * @throws Exception si création fail
+     */
+    public PayingEmployees() throws Exception
+    {
+	super(null);
+    }
+
+    public AbstractEntity doAction(AbstractEntity entity,
+	    Hashtable<String, String> parameters) throws Exception
+    {
+	int unpaid = 0;
+	PayableEmployee searchEmp = new PayableEmployee();
+	Collection<String> empId = parameters.values();
+
+	for (String id : empId)
+	{
+	    searchEmp.setData(new Employee().getForeignKeyName(), Integer
+		    .parseInt(id));
+	    AbstractOrmEntity employeeToPay = Orm.selectUnique(searchEmp);
+
+	    AlertEntity alert = (AlertEntity) new PayingEmployee().doAction(
+		    employeeToPay, null);
+	    if (alert != null)
+		unpaid++;
+
+	}
+	if (unpaid != 0)
+	    return new AlertEntity(
+		    String.valueOf(unpaid)
+			    + " employés n'ont pu être payés faute de fond\n vérifiez le compte correspondant au paiement");
+
+	return new AlertEntity(null);
+    }
+
+}
---
+++ NewtonERP/src/modules/finances/entityDefinitions/PayableEmployee.java
@@ -0,0 +1,88 @@
+package modules.finances.entityDefinitions;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+import modules.finances.actions.PayingEmployee;
+import modules.humanResources.entityDefinitions.Employee;
+import newtonERP.common.ActionLink;
+import newtonERP.module.AbstractOrmEntity;
+import newtonERP.orm.associations.AccessorManager;
+import newtonERP.orm.field.Field;
+import newtonERP.orm.field.Fields;
+import newtonERP.orm.field.type.FieldCurrency;
+import newtonERP.orm.field.type.FieldDate;
+import newtonERP.orm.field.type.FieldInt;
+import newtonERP.viewers.viewerData.ListViewerData;
+
+/**
+ * Entité PayableEmployee du module finances: représente les employes à payer et
+ * les montants payables à ceux-ci.
+ * 
+ * Sert également de donnée pour générer talon de paye...à venir
+ * 
+ * 
+ * @author Pascal Lemay
+ */
+public class PayableEmployee extends AbstractOrmEntity
+{
+    /**
+     * @throws Exception if creation fails
+     */
+    public PayableEmployee() throws Exception
+    {
+	super();
+	AccessorManager.addAccessor(this, new Employee());
+	AccessorManager.addAccessor(this, new StateType());
+	AccessorManager.addAccessor(this, new BankAccount());
+    }
+
+    @Override
+    public Fields initFields() throws Exception
+    {
+	Vector<Field<?>> fieldsInit = new Vector<Field<?>>();
+	FieldInt primaryKey = new FieldInt("Numéro", getPrimaryKeyName());
+	primaryKey.setNaturalKey(true);
+	fieldsInit.add(primaryKey);
+
+	fieldsInit.add(new FieldInt("Employee", new Employee()
+		.getForeignKeyName()));
+	// periode de paye
+	fieldsInit.add(new FieldDate("Début de période", "beginning"));
+	fieldsInit.add(new FieldDate("Fin de période", "end"));
+
+	fieldsInit.add(new FieldDate("Date de paiement", "paymentDate"));
+
+	fieldsInit.add(new FieldCurrency("Montant à payer", "balance"));
+
+	fieldsInit
+		.add(new FieldInt("État", new StateType().getForeignKeyName()));
+
+	fieldsInit.add(new FieldInt("Compte pour paiement", new BankAccount()
+		.getForeignKeyName()));
+
+	return new Fields(fieldsInit);
+    }
+
+    // To pay a single employee...with PayingEmployee--------------------------
+    // tempo, à voir si ce bouton restera là
+    @Override
+    public final ListViewerData getList(Hashtable<String, String> parameters)
+	    throws Exception
+    {
+	PayingEmployee paying = new PayingEmployee();
+	paying.setOwnedByModul(getCurrentModule());
+
+	Hashtable<String, String> actionParameters = new Hashtable<String, String>();
+	actionParameters.put(getPrimaryKeyName(), "&");
+
+	ListViewerData list = super.getList(parameters);
+
+	list.addSpecificActionButtonList(new ActionLink("Payer", paying,
+		actionParameters));
+
+	return list;
+    }
+    // ------------------------------------------------------------------
+
+}
