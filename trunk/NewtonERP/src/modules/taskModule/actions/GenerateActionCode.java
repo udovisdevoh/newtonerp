c4f6e7556c4b81cb9dc94623bbf84f25d3f009e2
glacasse <glacasse@f9981bd6-9353-11de-97c9-d5744c8abab9>
2009-12-02 20:24:38 +0000

    Code generator: générateur de stub d'action manuellement pour modules qui existent déjà.
    GetList de Field, ActionEntity et EntityEntity: enlevé bouton nouveau car les nouveaux field, actionEntity et EntityEntity doivent être créés par le bouton [+] de leur parent.

--- trunk/NewtonERP/src/modules/taskModule/actions/GenerateActionCode.java
+++ trunk/NewtonERP/src/modules/taskModule/actions/GenerateActionCode.java
@@ -1,10 +1,16 @@
 package modules.taskModule.actions;
 
+import java.io.File;
 import java.util.Hashtable;
 
 import modules.taskModule.entityDefinitions.ActionEntity;
+import modules.taskModule.entityDefinitions.ModuleEntity;
 import newtonERP.module.AbstractAction;
 import newtonERP.module.AbstractEntity;
+import newtonERP.orm.Orm;
+import newtonERP.sourceCodeBuilder.ActionSourceCodeBuilder;
+import newtonERP.sourceCodeBuilder.SourceCodeBuilder;
+import newtonERP.viewers.viewerData.BaseViewerData;
 
 /**
  * Sert à générer dynamiquement le code d'une action
@@ -24,7 +30,41 @@ public class GenerateActionCode extends AbstractAction
     public AbstractEntity doAction(AbstractEntity entity,
 	    Hashtable<String, String> parameters) throws Exception
     {
-	// TODO Auto-generated method stub
-	return null;
+	ActionEntity actionEntity = (ActionEntity) entity;
+
+	actionEntity = (ActionEntity) Orm.selectUnique(actionEntity);
+
+	String fileName = buildFileName(actionEntity);
+
+	BaseViewerData editUI = actionEntity.editUI(parameters);
+
+	if (!(new File(fileName).exists()))
+	{
+	    new File(getPackagePath(actionEntity)).mkdir();
+	    String sourceCode = ActionSourceCodeBuilder.build(actionEntity);
+	    GenerateSourceCode.writeClassFile(fileName, sourceCode);
+	    editUI.addAlertMessage("La classe de l'action a été créée.");
+	}
+	else
+	{
+	    editUI.addAlertMessage("La classe d'action existe déjà.");
+	}
+
+	return editUI;
+    }
+
+    private String buildFileName(ActionEntity actionEntity) throws Exception
+    {
+	String path = getPackagePath(actionEntity) + "/actions";
+	String fileName;
+	fileName = path + "/" + actionEntity.getDataString("systemName")
+		+ ".java";
+	return fileName;
+    }
+
+    private String getPackagePath(ActionEntity actionEntity) throws Exception
+    {
+	ModuleEntity moduleEntity = actionEntity.getModuleEntity();
+	return SourceCodeBuilder.getModulePackagePath(moduleEntity);
     }
 }
